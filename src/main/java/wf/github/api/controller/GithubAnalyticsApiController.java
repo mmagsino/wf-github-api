ca6f30755c687cc7a53deebc420536b185beb32f
Michael Magsino <michael.magsino@axa-asiassc.com>
2020-06-28 00:20:37 +0800

    Add swagger ui.

--- src/main/java/wf/github/api/controller/GithubAnalyticsApiController.java
+++ src/main/java/wf/github/api/controller/GithubAnalyticsApiController.java
@@ -1,5 +1,28 @@
 package wf.github.api.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Lists;
+
+import wf.github.api.model.Commit;
+import wf.github.api.model.Repository;
+
+@RestController
+@RequestMapping("/api")
 public class GithubAnalyticsApiController {
 
+	@GetMapping("/search")
+	public ResponseEntity<List<Repository>> repositories() {
+		return ResponseEntity.ok(Lists.newArrayList());
+	}
+
+	@GetMapping("/commits")
+	public ResponseEntity<List<Commit>> commits() {
+		return ResponseEntity.ok(Lists.newArrayList());
+	}
 }
